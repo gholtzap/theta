3b24db80ac76939bb4299bfb31e37f0befb19c63
gholtzap <gholtzap@asu.edu>
2023-08-07 01:26:39 -0400

    beta looks better now!! :D

--- src/components/IndexMakerForm.tsx
+++ src/components/IndexMakerForm.tsx
@@ -10,6 +10,9 @@ const IndexMakerForm = () => {
     const [tickerInput, setTickerInput] = useState('');
     const [indexName, setIndexName] = useState('');
     const [imageUrl, setImageUrl] = useState('');
+    const [title, setTitle] = useState('');
+    const [percentages, setPercentages] = useState({});
+
 
     const addTicker = () => {
         if (tickerInput && !tickers.includes(tickerInput)) {
@@ -39,17 +42,21 @@ const IndexMakerForm = () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            responseType: 'blob'
+            responseType: 'json'
         }
 
         try {
             const response = await axios.post('http://localhost:5000/beta', requestData, config)
-            const url = URL.createObjectURL(new Blob([response.data]))
-            setImageUrl(url)
+            const base64Image = `data:image/png;base64,${response.data.image}`
+            setImageUrl(base64Image)
+            setTitle(response.data.title)  // set the title
+            setPercentages(response.data.percentages)  // set the percentages
+
         } catch (error) {
             console.error('There was an error!', error)
         }
     }
+
 
     const handleTickerAddition = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter" && tickerInput.trim() !== "") {
@@ -60,52 +67,80 @@ const IndexMakerForm = () => {
 
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-950 text-white">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-950 text-white pt-40">
+            <div className="flex flex-col items-center justify-center min-h-screen bg-zinc-950 text-white">
 
-            <h1 className="mb-0 text-5xl font-bold">β</h1>
-            <h1 className="mb-8 text-2xl font-bold">Custom Index</h1>
+                <h1 className="mb-0 text-5xl font-bold">β</h1>
+                <h1 className="mb-8 text-2xl font-bold">Custom Index</h1>
 
-            {/* Index name input */}
-            <div className="w-full max-w-md relative rounded-lg mb-8 overflow-hidden border border-zinc-900/30">
-                <input
-                    type="text"
-                    value={indexName}
-                    onChange={(e) => setIndexName(e.target.value)}
-                    placeholder="Enter index name"
-                    className="w-full px-4 py-2 focus:outline-none text-black"
-                />
-            </div>
+                {/* Index name input */}
+                <div className="w-full max-w-md relative rounded-lg mb-8 overflow-hidden border border-zinc-900/30">
+                    <input
+                        type="text"
+                        value={indexName}
+                        onChange={(e) => setIndexName(e.target.value)}
+                        placeholder="Enter index name"
+                        className="w-full px-4 py-2 focus:outline-none text-black"
+                    />
+                </div>
 
-            {/* Ticker input */}
-            <div className="w-full max-w-md relative rounded-lg mb-8 overflow-hidden border border-zinc-900/30">
-                <input
-                    type="text"
-                    value={tickerInput}
-                    onChange={(e) => setTickerInput(e.target.value.toUpperCase())}
-                    onKeyPress={handleTickerAddition}
-                    placeholder="Add a ticker"
-                    className="w-full px-4 py-2 focus:outline-none text-black"
-                />
-                <button type="button" onClick={handleSubmit} className="absolute right-0 top-0 bg-white-500 text-white border-none px-6 py-3 scale-150">
-                    <img src="double_arrow.svg" alt="Submit" className="w-4 h-4" />
-                </button>
-            </div>
+                {/* Ticker input */}
+                <div className="w-full max-w-md relative rounded-lg mb-8 overflow-hidden border border-zinc-900/30">
+                    <input
+                        type="text"
+                        value={tickerInput}
+                        onChange={(e) => setTickerInput(e.target.value.toUpperCase())}
+                        onKeyPress={handleTickerAddition}
+                        placeholder="Add a ticker"
+                        className="w-full px-4 py-2 focus:outline-none text-black"
+                    />
+                    <button type="button" onClick={handleSubmit} className="absolute right-0 top-0 bg-white-500 text-white border-none px-6 py-3 scale-150">
+                        <img src="double_arrow.svg" alt="Submit" className="w-4 h-4" />
+                    </button>
+                </div>
 
-            {/* Ticker list */}
-            <div className="w-3/4 grid grid-cols-6 gap-2 mb-8">
-                {tickers.map((ticker, index) => (
-                    <div key={index} className="w-40 h-10 p-6 m-4 text-white rounded-xl border border-zinc-500 flex justify-between items-center">
-                        <h2 className="text-xl font-semibold">{ticker}</h2>
-                        <button onClick={() => removeTicker(index)} className="btn">X</button>
+                {/* Ticker list */}
+                <div className="w-3/4 flex flex-wrap gap-2 mb-8">
+                    {tickers.map((ticker, index) => (
+                        <div key={index} className="w-40 h-10 p-6 m-4 text-white rounded-xl border border-zinc-500 flex justify-between items-center">
+                            <h2 className="text-xl font-semibold">{ticker}</h2>
+                            <button onClick={() => removeTicker(index)} className="btn">X</button>
+                        </div>
+                    ))}
+                </div>
+
+                {/* Display results */}
+
+
+                <div className="w-full flex justify-center">
+
+                    <div className="w-1/4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mb-8 auto-rows-max grid-flow-row-dense">
+                        {Object.entries(percentages)
+                            .sort(([, a], [, b]) => b - a)  // sort in descending order
+                            .map(([ticker, percentage]) => (
+                                <div key={ticker} className="mb-4">
+                                    <span className="inline-block w-24 text-white">{ticker}:</span>
+                                    <div className="bg-white bg-opacity-20 h-6 rounded overflow-hidden inline-block w-3/4 border border-white">
+                                        <div
+                                            className="h-full bg-white"
+                                            style={{ width: `${percentage}%` }}  // scale the inner div by the percentage
+                                        ></div>
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
-                ))}
-            </div>
 
-            {/* Display results */}
-            <div className="w-full flex justify-center">
-                {imageUrl && (
-                    <img src={imageUrl} alt="Generated graph" className="w-3/4 md:w-1/2 lg:w-1/3" />
-                )}
+                    {imageUrl && (
+                        <div className="flex flex-col items-center">
+                            <h2 className="text-2xl mb-4 text-center">{title}</h2>
+                            <img src={imageUrl} alt="Generated graph" className="w-1.5 md:w-1 lg:w-2/3 mx-auto" />
+                        </div>
+                    )}
+
+                </div>
+
+
             </div>
         </div>
     );
