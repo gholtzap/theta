fd0e0bfb9a1d7b0808ca2bf1540d36b07063b152
gholtzap <gholtzap@asu.edu>
2023-09-12 19:44:26 -0700

    more typescipt specific fixes for deployment

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import Header from './Header';
 
-const Layout: React.FC = ({ children }) => {
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div>
       <Header />
